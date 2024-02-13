3ad63ec429fae61ad9410e2c7b95831e323e074f
Marcin Barc <marb637@gmail.com>
2024-02-13 20:29:59 +0100

    types and mockup added

---
+++ src/data/todos.ts
@@ -0,0 +1,5 @@
+export const dummyData = [
+    { id: 1, title: 'Like this video', completed: false },
+    { id: 2, title: 'Subscribe this video', completed: false },
+    { id: 3, title: 'Leave a nice comment', completed: false },
+];
