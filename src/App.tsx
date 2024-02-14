a1b8af9b5c3a177220013282230642858814b740
Marcin Barc <marb637@gmail.com>
2024-02-14 19:21:36 +0100

    hook useTodos added

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { dummyData } from './data/todos';
 import { AddTodoForm } from './components/AddTodoForm';
 import { TodoList } from './components/TodoList';
 import { TodoSummary } from './components/TodoSummary';
-import { TodoItemProps } from './types/todo';
+import { useTodos } from './hooks/useTodos';
 
 function App() {
-    const [todos, setTodos] = useState(() => {
-        const savedTodos: TodoItemProps[] = JSON.parse(
-            localStorage.getItem('todos') || '[]',
-        );
-        return savedTodos.length > 0 ? savedTodos : dummyData;
-    });
-
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos));
-    }, [todos]);
-
-    const setTodoCompleted = (id: number, completed: boolean) =>
-        setTodos((prevState) =>
-            prevState.map((todo) =>
-                todo.id === id ? { ...todo, completed } : todo,
-            ),
-        );
-
-    const addToto = (title: string) => {
-        setTodos((prevState) => [
-            { id: prevState.length + 1, title, completed: false },
-            ...prevState,
-        ]);
-    };
-
-    const deleteTodo = (id: number) => {
-        setTodos((prevState) => prevState.filter((todo) => todo.id !== id));
-    };
-
-    const deleteTodoCompleted = () => {
-        setTodos((prevState) => prevState.filter((todo) => !todo.completed));
-    };
+    const {
+        todos,
+        setTodoCompleted,
+        addToto,
+        deleteTodo,
+        deleteTodoCompleted,
+    } = useTodos();
 
     return (
         <main className='py-10 h-screen space-y-5 overflow-y-auto'>
